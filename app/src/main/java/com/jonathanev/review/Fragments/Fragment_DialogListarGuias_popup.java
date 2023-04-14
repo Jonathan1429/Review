3e87f0a19b2ff511e72b832f3fd12df708d576cd
Jonathan EV <jescobar@lamanicurista.com>
2023-04-14 00:31:17 -0600

    Se corrigieron bugs en modificación de guias, implementaciones e implementación de comentarios

---
+++ app/src/main/java/com/jonathanev/review/Fragments/Fragment_DialogListarGuias_popup.java
@@ -0,0 +1,233 @@
+package com.jonathanev.review.Fragments;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
+
+import com.jonathanev.review.Activities.Activity_Modificar;
+import com.jonathanev.review.Activities.Activity_RepasarGuia;
+import com.jonathanev.review.Clases.Guias;
+import com.jonathanev.review.Fragments.Adaptadores.AdaptadorPersonalizadoListarGuias;
+import com.jonathanev.review.R;
+import com.jonathanev.review.databinding.FragmentListarGuiasBinding;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Set;
+
+public class Fragment_DialogListarGuias_popup extends DialogFragment {
+
+    private FragmentListarGuiasBinding binding;
+
+    // Guardar la collection del set en este Arreglo
+    private ArrayList<String> item = new ArrayList<String>();
+
+    // Cargar el ListView
+    private ArrayList<Guias> lista = new ArrayList<>();
+
+    public Fragment_DialogListarGuias_popup() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentListarGuiasBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Recibimos las preferencias y las guardamos nuevamente en un set.
+        SharedPreferences preferences = getActivity().getSharedPreferences("nombres_guias", Context.MODE_PRIVATE);
+        Set<String> set = preferences.getStringSet("guias_estudio", null);
+
+        // Metemos la collection directamente en el arreglo y se ordena automáticamente.
+        item.addAll(set);
+
+        // Ordena las guías con el método sort
+        item.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        // Sino tengo ninguna guía de estudio aparece el texto que no tengo guías
+        if (item.isEmpty()){
+            binding.tvSinGuias.setVisibility(View.VISIBLE);
+        } else {
+            // Si es lo contrario no aparece el texto
+            binding.tvSinGuias.setVisibility(View.INVISIBLE);
+        }
+
+        // Cargamos la lista de los items
+        cargarListaGuiasEstudio(item);
+
+        binding.lvGuiasEstudio.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int posicion, long id) {
+                Guias guias = lista.get(posicion);
+
+                // Creo una alerta donde me saldrán una lista de items
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setIcon(R.drawable.ic_advertencia);
+                builder.setTitle("¿Qué acción deseas realizar?");
+                builder.setItems(new CharSequence[]
+                                {"Abrir Guia", "Modificar Guia", "Eliminar Guia", "Cancelar"},
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case 0:
+                                        // Si entra al primer item se abre la guía a repasar
+                                        Intent intentAbrirGuia = new Intent(getActivity(), Activity_RepasarGuia.class);
+                                        intentAbrirGuia.putExtra("nombre_archivo", guias.getNombreGuia());
+                                        startActivity(intentAbrirGuia);
+                                        // Recuperamos el dialogo abierto actualmente
+                                        // (Fragment_DialogListarGuias.java) y lo cerramos.
+                                        Dialog dialogoAbrirGuia =  getDialog();
+                                        dialogoAbrirGuia.dismiss();
+                                        break;
+                                    case 1:
+                                        // Si entra al segundo es para modificar la guía de estudio
+                                        Intent intentModificarGuia = new Intent(getActivity(), Activity_Modificar.class);
+                                        intentModificarGuia.putExtra("nombre_archivo", guias.getNombreGuia());
+                                        startActivity(intentModificarGuia);
+                                        // Recuperamos el dialogo abierto actualmente
+                                        // (Fragment_DialogListarGuias.java) y lo cerramos.
+                                        Dialog dialogoModificarGuia =  getDialog();
+                                        dialogoModificarGuia.dismiss();
+                                        break;
+                                    case 2:
+                                        // Se ejecuta cuando se regresa sin guardar.
+                                        new AlertDialog.Builder(getContext())
+                                                .setTitle("¡Atención!")
+                                                .setMessage("¿Estás seguro que deseas eliminar la" +
+                                                        " guia?")
+                                                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                                        // Si entra al tercero es para eliminar la guia exitosamente
+                                                        @SuppressLint("SdCardPath") File file = new File("/data/data/com.jonathanev.repasar/files/");
+                                                        if (file.exists()){
+                                                            new File(file, guias.getNombreGuia()+".xml").delete();
+                                                            Toast.makeText(getContext(),
+                                                                    "¡Archivo eliminado exitosamente!",
+                                                                    Toast.LENGTH_SHORT).show();
+
+                                                            // Recuperamos el dialogo abierto actualmente
+                                                            // (Fragment_DialogListarGuias_popup.java)
+                                                            // y lo cerramos.
+                                                            Dialog dialogoEliminarGuia =  getDialog();
+                                                            dialogoEliminarGuia.dismiss();
+                                                        } else {
+                                                            Toast.makeText(getContext(), "La ruta para eliminar el " +
+                                                                            "archivo actualmente no existe.",
+                                                                    Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                })
+                                                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int i) {
+                                                        dialog.dismiss();
+                                                    }
+                                                }).create().show();
+                                        break;
+                                    case 3:
+                                        // Si entra al 5 se cancela cualquier operación
+                                        dialog.dismiss();
+                                        Toast.makeText(getContext(), "Cancelaste la acción", Toast.LENGTH_SHORT).show();
+                                        break;
+                                }
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+    }
+
+    private void cargarListaGuiasEstudio(ArrayList<String> item) {
+        // Se crea un número random para saber cual imagen aparecerá en el listado.
+        int numeroRandom = (int) (Math.random() * 6) + 1;
+
+        switch (numeroRandom){
+            case 1:
+                numeroRandom = R.drawable.cerebro;
+                break;
+            case 2:
+                numeroRandom = R.drawable.cuaderno;
+                break;
+            case 3:
+                numeroRandom = R.drawable.estudiar;
+                break;
+            case 4:
+                numeroRandom = R.drawable.libro_dual;
+                break;
+            case 5:
+                numeroRandom = R.drawable.libro_triple;
+                break;
+            case 6:
+                numeroRandom = R.drawable.libros;
+                break;
+        }
+
+        AdaptadorPersonalizadoListarGuias adaptador = null;
+        Guias guias;
+
+        // La imagen guardada en numeroRandom se pondrá en el objeto guias
+        // Después se irá poniendo el nombre de cada una de las guias en el objeto
+        for (int i = 0; i < item.size(); i++) {
+            guias = new Guias();
+            guias.setImgGuia(numeroRandom);
+            guias.setNombreGuia(item.get(i));
+            lista.add(guias);
+        }
+
+        adaptador = new AdaptadorPersonalizadoListarGuias(getContext(), lista);
+        binding.lvGuiasEstudio.setAdapter(adaptador);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Dialog dialog = getDialog();
+        if (dialog != null){
+            dialog.getWindow().setLayout(900, 1200);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+}
---
+++ app/src/main/java/com/jonathanev/review/Fragments/Fragment_DialogNuevoArchivo_popu.java
@@ -0,0 +1,142 @@
+package com.jonathanev.review.Fragments;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.jonathanev.review.Activities.Activity_Cuestionario;
+import com.jonathanev.review.databinding.FragmentNuevoArchivoBinding;
+
+import java.io.File;
+
+public class Fragment_DialogNuevoArchivo_popu extends DialogFragment{
+
+    private FragmentNuevoArchivoBinding binding;
+
+    public Fragment_DialogNuevoArchivo_popu() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentNuevoArchivoBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnGuardarGuiaEstudio.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SdCardPath")
+            @Override
+            public void onClick(View view) {
+                String item = "";
+                // Defino la ruta donde busco los ficheros.
+                @SuppressLint("SdCardPath") File file = new File("/data/data/com.jonathanev.review/files/");
+
+                boolean archivoExiste = false;
+                if (file.exists()){
+                    // Creo el array de tipo File con el contenido de la carpeta.
+                    File[] files = file.listFiles();
+                    File archivo = null;
+                    // Hacemos un ciclo por cada fichero para extraer el nombre uno a uno.
+                    for (int i = 0; i < files.length; i++){
+                        // Sacamos del array files el nombre recuperandolo por posición.
+                        archivo = files[i];
+                        item = archivo.getName().replaceAll(".xml", "");
+                        // Comparamos el texto ingresado en la App con el recuperado.
+                        if (binding.etNombreArchivo.getText().toString().equals(item)){
+                            archivoExiste = true;
+                            break;
+                        }
+                    }
+
+                    // Si hay un archivo existente entra
+                    if (archivoExiste){
+                        // Se ejecuta cuando se regresa sin guardar.
+                        new AlertDialog.Builder(getContext())
+                                .setTitle("¡Atención!")
+                                .setMessage("Ya tienes una guía con el mismo nombre, " +
+                                        "si continuas se va a sobreescribir el archivo, " +
+                                        "¿seguro deseas continuar?")
+                                .setPositiveButton("Continuar", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        // Si hay un valor dentro del campo enviamos el nombre del archivo a
+                                        // Activity_Cuestionario.
+                                        Intent intent = new Intent(getActivity(), Activity_Cuestionario.class);
+                                        intent.putExtra("nombre_archivo", binding.etNombreArchivo.getText().toString());
+                                        // Recuperamos el dialogo abierto actualmente
+                                        // (Fragment_DialogNuevoArchivo.java) y lo cerramos.
+                                        Dialog dialogActual =  getDialog();
+                                        dialogActual.dismiss();
+                                        binding.etNombreArchivo.setText("");
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int i) {
+                                        dialog.dismiss();
+                                    }
+                                }).create().show();
+                    } else { // Sino hay un archivo existente entra aquí
+                        // Si hay un valor dentro del campo enviamos el nombre del archivo a
+                        // Activity_Cuestionario.
+                        if (binding.etNombreArchivo.getText().toString().isEmpty()){
+                            Toast.makeText(getContext(),
+                                    "Tienes que colocar un nombre para el archivo",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Intent intent = new Intent(getActivity(), Activity_Cuestionario.class);
+                            intent.putExtra("nombre_archivo", binding.etNombreArchivo.getText().toString());
+                            // Recuperamos el dialogo abierto actualmente
+                            // (Fragment_DialogNuevoArchivo.java) y lo cerramos.
+                            Dialog dialogActual =  getDialog();
+                            dialogActual.dismiss();
+                            binding.etNombreArchivo.setText("");
+                            startActivity(intent);
+                        }
+                    }
+                }
+
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Dialog dialog = getDialog();
+        if (dialog != null){
+            dialog.getWindow().setLayout(700, 700);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+}
