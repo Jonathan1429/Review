c96b573a45a75a7f93f9025f97c6a80104c7cab2
Jonathan EV <jescobar@lamanicurista.com>
2023-04-15 21:09:01 -0600

    Implementación de los botones Eliminar y Atrás cuando recién creas la guía

--- app/src/main/java/com/jonathanev/review/Activities/Activity_Cuestionario.java
+++ app/src/main/java/com/jonathanev/review/Activities/Activity_Cuestionario.java
@@ -34,11 +34,15 @@ import org.xmlpull.v1.XmlSerializer;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Activity_Cuestionario extends AppCompatActivity {
 
     private ActivityCuestionarioBinding binding;
     private String nombreArchivo;
+    private ArrayList<String> preguntas = new ArrayList<>();
+    private ArrayList<String> respuestas = new ArrayList<>();
+    int contador = 0;
     private InterstitialAd mInterstitialAd;
 
     // Creamos la serialización y la clase para crear archivos de manera global.
@@ -50,19 +54,9 @@ public class Activity_Cuestionario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityCuestionarioBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        final boolean[] isUpdating = {false};
-        final String[] colorLetra = {""};
-        final int[] color = {Color.RED};
-
-        // Crear un SpannableStringBuilder vacío y una referencia a un objeto ForegroundColorSpan
-        SpannableStringBuilder builder = new SpannableStringBuilder();
-        final ForegroundColorSpan[] lastColorSpan = {null};
-        // Definir la oración y los colores para cada carácter
-        String sentence = "Hola mundo feliz";
-        final int[][] colors = {new int[]{Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.MAGENTA, Color.CYAN, Color.GRAY, Color.DKGRAY, Color.LTGRAY, Color.BLACK}};
 
         // Sección de anuncios
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+        /*MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
@@ -88,18 +82,55 @@ public class Activity_Cuestionario extends AppCompatActivity {
                         Log.i(TAG, loadAdError.getMessage());
                         mInterstitialAd = null;
                     }
-                });
+                });*/
 
         // Recibimos el nombre del archivo del popupFragment Nueva Guia.
         nombreArchivo = getIntent().getExtras().getString("nombre_archivo");
-
-        // Creamos el archivo una vez que entra al Activity de cuestionario.
-        crearArchivo(nombreArchivo);
 
         binding.barraSuperiorRegreso.imgvBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cancelarArchivo();
+            }
+        });
+
+        binding.btnAtrasPregunta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // El contador tiene que ser mayor a 0 sino significa que no hay preguntas anteriores.
+                if (contador > 0){
+                    // Se le quita 1 para hacer referencia al arreglo
+                    // tamaño 3-1 = 2 [0,1,2].
+                    int longi = preguntas.size()-1;
+
+                    // Contador tendrá acceso a modificar lo que esté en el rango a excepción
+                    // de lo que esté en la posición 0.
+                    if (contador <= longi){
+                        // Validamos campos vacios en la pregunta y respuesta.
+                        if (binding.etPregunta.getText().toString().isEmpty()
+                                || binding.etRespuesta.getText().toString().isEmpty()){
+                            Toast.makeText(getApplicationContext(),
+                                    "Asegurate de no dejar ningun campo vacio",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Si los campos están bien se sobre escribe.
+                            preguntas.set(contador, binding.etPregunta.getText().toString());
+                            respuestas.set(contador, binding.etRespuesta.getText().toString());
+
+                            binding.etPregunta.setText(preguntas.get(contador-1));
+                            binding.etRespuesta.setText(respuestas.get(contador-1));
+                        }
+                    } else {
+                        // Si el contador es mayor a lo que hay guardado entonces únicamente
+                        // escribirá en los textos.
+                        binding.etPregunta.setText(preguntas.get(contador-1));
+                        binding.etRespuesta.setText(respuestas.get(contador-1));
+                    }
+                    contador--;
+                } else {
+                    Toast.makeText(getApplicationContext(), "Ya no tienes preguntas anteriores"
+                            , Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -113,30 +144,128 @@ public class Activity_Cuestionario extends AppCompatActivity {
                             "Asegurate de no dejar ningun campo vacio",
                             Toast.LENGTH_SHORT).show();
                 } else {
-                    // Guardamos la pregunta y respuesta en una variable
-                    String pregunta = binding.etPregunta.getText().toString();
-                    String respuesta = binding.etRespuesta.getText().toString();
+                    // Se le quita 1 para hacer referencia al arreglo
+                    // tamaño 3-1 = 2 [0,1,2].
+                    int longi = preguntas.size()-1;
 
-                    // Enviamos los valores al método para escribir en el XML.
-                    escribirPreguntaXml(pregunta, respuesta);
-                    binding.btnGuardarGuiaInhabilitado.setVisibility(View.INVISIBLE);
-                    binding.btnGuardarGuia.setVisibility(View.VISIBLE);
-                    binding.etPregunta.setText("");
-                    binding.etRespuesta.setText("");
-                    binding.etPregunta.requestFocus();
+                    // Contador tendrá acceso a modificar lo que esté en el rango a excepción
+                    // de lo que esté en la posición 0.
+                    if (contador <= longi){
+                        preguntas.set(contador, binding.etPregunta.getText().toString());
+                        respuestas.set(contador, binding.etRespuesta.getText().toString());
+
+                        // Mientras el contador sea menor escribiremos la siguiente pregunta
+                        // en los et.
+                        if (contador < longi){
+                            binding.etPregunta.setText(preguntas.get(contador+1));
+                            binding.etRespuesta.setText(respuestas.get(contador+1));
+                        } else {
+                            // Si el contador es igual entonces solo escribiremos los campos vacios.
+                            binding.etPregunta.setText("");
+                            binding.etRespuesta.setText("");
+                            binding.etPregunta.requestFocus();
+                        }
+                    } else {
+                        // Si el contador es mayor entonces agregaremos la pregunta actual a los
+                        // arreglos.
+                        preguntas.add(contador, binding.etPregunta.getText().toString());
+                        respuestas.add(contador, binding.etRespuesta.getText().toString());
+
+                        binding.etPregunta.setText("");
+                        binding.etRespuesta.setText("");
+                        binding.etPregunta.requestFocus();
+                    }
+                    contador++;
                 }
+            }
+        });
+
+        binding.btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(Activity_Cuestionario.this)
+                        .setTitle("¡Atención!")
+                        .setMessage("¿Quieres eliminar la pregunta?")
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // Se le quita 1 para hacer referencia al arreglo
+                                // tamaño 3-1 = 2 [0,1,2].
+                                int longi = preguntas.size()-1;
+
+                                // Contador tendrá acceso a modificar lo que esté en el rango a excepción
+                                // de lo que esté en la posición 0.
+                                if (contador <= longi){
+                                    preguntas.remove(contador);
+                                    respuestas.remove(contador);
+
+                                    // Mientras el contador sea menor escribiremos la siguiente pregunta
+                                    // en los et.
+                                    if (contador < longi){
+                                        binding.etPregunta.setText(preguntas.get(contador));
+                                        binding.etRespuesta.setText(respuestas.get(contador));
+                                    } else {
+                                        // Si el contador es igual entonces solo escribiremos los campos vacios.
+                                        binding.etPregunta.setText("");
+                                        binding.etRespuesta.setText("");
+                                        binding.etPregunta.requestFocus();
+                                    }
+                                } else {
+                                    // Si el contador es mayor entonces únicamente limpiamos los campos.
+                                    binding.etPregunta.setText("");
+                                    binding.etRespuesta.setText("");
+                                    binding.etPregunta.requestFocus();
+                                }
+                            }
+                        })
+                        .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.dismiss();
+                            }
+                        }).create().show();
             }
         });
 
         binding.btnGuardarGuia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Guardamos lo que se escriba en la pregunta y respuesta.
-                String pregunta = binding.etPregunta.getText().toString();
-                String respuesta = binding.etRespuesta.getText().toString();
+                // Se le quita 1 para hacer referencia al arreglo
+                // tamaño 3-1 = 2 [0,1,2].
+                int longi = preguntas.size()-1;
 
-                // Dentro del método valido si los campos son vacios.
-                guardarXml(pregunta, respuesta);
+                // Si alguno de los dos campos está vacio entra aquí.
+                if (!binding.etPregunta.getText().toString().isEmpty() &&
+                    binding.etRespuesta.getText().toString().isEmpty() ||
+                    binding.etPregunta.getText().toString().isEmpty() &&
+                    !binding.etRespuesta.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),
+                            "Asegurate de no dejar ningún campo vacio",
+                            Toast.LENGTH_SHORT).show();
+                } else if (binding.etPregunta.getText().toString().isEmpty()
+                        && binding.etRespuesta.getText().toString().isEmpty()){
+                    // Si los dos campos están vacios entra aquí.
+
+                    if (contador == 0){
+                        Toast.makeText(getApplicationContext(),
+                                "¡No puedes guardar una guía sin datos!",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        crearArchivo(nombreArchivo);
+                    }
+                } else if (contador > longi){
+                    // Si el contador es mayor a lo guardado entonces agregamos la pregunta
+                    // anteriormente ya validamos campos vacios.
+                    preguntas.add(contador, binding.etPregunta.getText().toString());
+                    respuestas.add(contador, binding.etRespuesta.getText().toString());
+                    crearArchivo(nombreArchivo);
+                } else {
+                    // Si el contador no es mayor a lo guardado entonces modificamos lo actual en
+                    // el arreglo, además anteriormente ya validamos campos vacios.
+                    preguntas.set(contador, binding.etPregunta.getText().toString());
+                    respuestas.set(contador, binding.etRespuesta.getText().toString());
+                    crearArchivo(nombreArchivo);
+                }
             }
         });
     }
@@ -191,57 +320,27 @@ public class Activity_Cuestionario extends AppCompatActivity {
 
                 serializer.startTag("", "Cuestionario");
                     serializer.attribute("", "nombreGuia", nombreArchivo);
-                    // serializer.text("Hola");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void escribirPreguntaXml(String pregunta, String respuesta) {
-        try {
-                // Creo la etiqueta interrogante con su respectiva pregunta
-                serializer.startTag("", "Interrogante");
-                    serializer.attribute("", "pregunta", pregunta);
-                    serializer.attribute("", "respuesta", respuesta);
-                serializer.endTag("", "Interrogante");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void guardarXml(String pregunta, String respuesta){
-        try {
-            // Valido que ambos campos esten vacios o llenos sino envío un mensaje.
-            if (pregunta.isEmpty() && !respuesta.isEmpty() ||
-            respuesta.isEmpty() && !pregunta.isEmpty()){
-                Toast.makeText(getApplicationContext(),
-                        "Llena o vacía ambos campos para poder continuar",
-                        Toast.LENGTH_SHORT).show();
-            } else{
-                // Aquí verifico que cuando le de a guardar los campos no se encuentren vacios,
-                // si ambos campos están con valores lo guardo sino se ignora.
-                if (!pregunta.isEmpty() && !respuesta.isEmpty()){
-                    serializer.startTag("", "Interrogante");
-                    serializer.attribute("", "pregunta", pregunta);
-                    serializer.attribute("", "respuesta", respuesta);
-                    serializer.endTag("", "Interrogante");
-                }
-
+                    // Creo la etiqueta interrogante con su respectiva pregunta
+                    for (int i = 0; i < preguntas.size(); i++) {
+                        serializer.startTag("", "Interrogante");
+                        serializer.attribute("", "pregunta", preguntas.get(i));
+                        serializer.attribute("", "respuesta", respuestas.get(i));
+                        serializer.endTag("", "Interrogante");
+                    }
                 // Si los campos estan vacios simplemente cierro las etiquetas y directamente
                 // guardo el documento en el teléfono.
                 serializer.endTag("", "Cuestionario");
-                serializer.endTag("", "GuiaEstudio");
-                serializer.endDocument();
-                serializer.flush();
-                fos.close();
-                Toast.makeText(getApplicationContext(), "Guia de estudio creada exitosamente",
-                        Toast.LENGTH_SHORT).show();
+            serializer.endTag("", "GuiaEstudio");
+            serializer.endDocument();
+            serializer.flush();
+            fos.close();
+            Toast.makeText(getApplicationContext(), "Guia de estudio creada exitosamente",
+                    Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(getApplicationContext(), Activity_RepasarGuia.class);
-                intent.putExtra("nombre_archivo", nombreArchivo);
-                startActivity(intent);
-                finish();
-            }
+            Intent intent = new Intent(getApplicationContext(), Activity_RepasarGuia.class);
+            intent.putExtra("nombre_archivo", nombreArchivo);
+            startActivity(intent);
+            finish();
         } catch (IOException e) {
             e.printStackTrace();
         }
