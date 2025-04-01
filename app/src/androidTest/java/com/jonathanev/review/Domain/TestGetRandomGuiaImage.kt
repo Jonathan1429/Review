812b418a5af741b058bffa07c05d88f1e4eb9e00
Jonathan EV <jona142959@gmail.com>
2025-04-01 15:23:07 -0600

    Se agregó el coverage para los TestUnitario y TestUI

--- app/src/androidTest/java/com/jonathanev/review/Domain/TestGetRandomGuiaImage.kt
+++ app/src/androidTest/java/com/jonathanev/review/Domain/TestGetRandomGuiaImage.kt
@@ -14,13 +14,24 @@ class TestGetRandomGuiaImage {
     private val getRandomGuiaImageUseCase = GetRandomGuiaImageUseCase()
 
     @Test
-    fun should_return_correct_drawable_based_on_random_number() {
+    fun regresa_la_imagen_1() {
         mockkObject(Random)  // Mockeamos el objeto Random
 
-        every { Random.nextInt(1, 7) } returns 1  // Forzamos que devuelva 3
+        every { Random.nextInt(1, 7) } returns 1  // Forzamos que devuelva 1
 
         val result = getRandomGuiaImageUseCase.invoke()
 
-        assertEquals(R.drawable.img_estudiante1, result)  // Verificamos que devuelve la imagen esperada
+        assertEquals(R.drawable.img_estudiante1, result)  // 1-6 Verificamos que devuelve la imagen esperada
+    }
+
+    @Test
+    fun regresa_la_imagen_2() {
+        mockkObject(Random)  // Mockeamos el objeto Random
+
+        every { Random.nextInt(1, 7) } returns 2  // 1-6 Forzamos que devuelva 2
+
+        val result = getRandomGuiaImageUseCase.invoke()
+
+        assertEquals(R.drawable.img_estudiante2, result)  // Verificamos que devuelve la imagen esperada
     }
 }
