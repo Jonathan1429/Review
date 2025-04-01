4a313cee3b546a8881118065ca31b1da79dd3445
Jonathan EV <jona142959@gmail.com>
2025-04-01 17:15:44 -0600

    creacion del resto de test para getRandomGuiaImage

--- app/src/androidTest/java/com/jonathanev/review/Domain/TestGetRandomGuiaImage.kt
+++ app/src/androidTest/java/com/jonathanev/review/Domain/TestGetRandomGuiaImage.kt
@@ -34,4 +34,48 @@ class TestGetRandomGuiaImage {
 
         assertEquals(R.drawable.img_estudiante2, result)  // Verificamos que devuelve la imagen esperada
     }
+
+    @Test
+    fun regresa_la_imagen_3() {
+        mockkObject(Random)  // Mockeamos el objeto Random
+
+        every { Random.nextInt(1, 7) } returns 3  // Forzamos que devuelva 3
+
+        val result = getRandomGuiaImageUseCase.invoke()
+
+        assertEquals(R.drawable.img_estudiante3, result)  // 1-6 Verificamos que devuelve la imagen esperada
+    }
+
+    @Test
+    fun regresa_la_imagen_4() {
+        mockkObject(Random)  // Mockeamos el objeto Random
+
+        every { Random.nextInt(1, 7) } returns 4  // 1-6 Forzamos que devuelva 4
+
+        val result = getRandomGuiaImageUseCase.invoke()
+
+        assertEquals(R.drawable.img_estudiante4, result)  // Verificamos que devuelve la imagen esperada
+    }
+
+    @Test
+    fun regresa_la_imagen_5() {
+        mockkObject(Random)  // Mockeamos el objeto Random
+
+        every { Random.nextInt(1, 7) } returns 5  // Forzamos que devuelva 5
+
+        val result = getRandomGuiaImageUseCase.invoke()
+
+        assertEquals(R.drawable.img_estudiante5, result)  // 1-6 Verificamos que devuelve la imagen esperada
+    }
+
+    @Test
+    fun regresa_la_imagen_6() {
+        mockkObject(Random)  // Mockeamos el objeto Random
+
+        every { Random.nextInt(1, 7) } returns 6  // 1-6 Forzamos que devuelva 6
+
+        val result = getRandomGuiaImageUseCase.invoke()
+
+        assertEquals(R.drawable.img_estudiante6, result)  // Verificamos que devuelve la imagen esperada
+    }
 }
