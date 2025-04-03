3739dd70e5fc87379533c7f91c327fcbd6901899
Jonathan EV <jona142959@gmail.com>
2025-04-03 10:54:52 -0600

    creacion del test get_guia_posicion_use_case

---
+++ app/src/test/java/com/jonathanev/review/Domain/TestGetGuiaPosicionUseCase.kt
@@ -0,0 +1,27 @@
+package com.jonathanev.review.Domain
+
+import com.jonathanev.review.Data.Model.GuiaModel
+import com.jonathanev.review.Data.Model.GuiaProvider
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TestGetGuiaPosicionUseCase {
+    private val guiaProvider = mockk<GuiaProvider>()
+    private var getGuiaPosicionUseCase = GetGuiaPosicionUseCase(guiaProvider)
+
+    @Test
+    fun `te regresa la guia en la posicion enviada que se encuentra en el provider`() {
+        //val guias = mockk<GuiaProvider>()
+        every { guiaProvider.guias } returns
+                listOf(
+                    GuiaModel("a", 0, true),
+                    GuiaModel("b", 0, true)
+                )
+
+        getGuiaPosicionUseCase = GetGuiaPosicionUseCase(guiaProvider)
+        val resultado = getGuiaPosicionUseCase(0)
+        assertEquals(GuiaModel("a", 0, true), resultado)
+    }
+}
