98cdca5f13e9ce11ff96d3b82987b54ea21ea402
Jonathan EV <jona142959@gmail.com>
2025-04-03 14:18:38 -0600

    creacion del test completo

---
+++ app/src/test/java/com/jonathanev/review/Domain/TestSetCifrarRutaImagenUseCase.kt
@@ -0,0 +1,28 @@
+package com.jonathanev.review.Domain
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TestSetCifrarRutaImagenUseCase {
+    private val setCifrarRutaImagenUseCase = SetCifrarRutaImagenUseCase()
+
+    @Test
+    fun `Cuando enviamos un texto debe regresarte todo el texto cifrado`(){
+        val texto = "Hola"
+        val desplazamiento = 3
+
+        val result = setCifrarRutaImagenUseCase(texto, desplazamiento)
+        assertEquals("Krod", result)
+        //println((('l' - 'a' + 3) % 26 + 'a'.code).toChar())
+    }
+
+    @Test
+    fun `Cuando enviamos un texto y este contiene numeros solo cambiara las letras`(){
+        val texto = "H1o3l4a"
+        val desplazamiento = 3
+
+        val result = setCifrarRutaImagenUseCase(texto, desplazamiento)
+        assertEquals("K1r3o4d", result)
+        //println((('l' - 'a' + 3) % 26 + 'a'.code).toChar())
+    }
+}
