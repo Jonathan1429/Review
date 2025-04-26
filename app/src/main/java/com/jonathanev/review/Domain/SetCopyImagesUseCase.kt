f6ddc9d0828ea4026dc717103729c49af1445758
Jonathan EV <jona142959@gmail.com>
2025-04-26 13:01:13 -0600

    creacion del test y modificacion del useCase

--- app/src/main/java/com/jonathanev/review/Domain/SetCopyImagesUseCase.kt
+++ app/src/main/java/com/jonathanev/review/Domain/SetCopyImagesUseCase.kt
@@ -4,30 +4,21 @@ import com.jonathanev.review.Core.Constants.fileImages
 import com.jonathanev.review.Core.Constants.fileImagesPiv
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 import javax.inject.Inject
 
 class SetCopyImagesUseCase @Inject constructor() {
-    operator fun invoke() {
-        val images = fileImagesPiv.listFiles()
-        // Hacemos un ciclo por cada fichero para extraer el nombre de cada uno.
-        if (!images.isNullOrEmpty()) {
-            for (i in images.indices) {
-                // Sacamos del array files el primer fichero.
-                val archivo: File = images[i]
-                var name = ""
-
-                name = archivo.name
-
+    operator fun invoke(imgPiv: File = fileImagesPiv, imagenes: File = fileImages) {
+        val images = imgPiv.listFiles()
+        if (images != null) {
+            for (archivo in images) {
+                val name = archivo.name
                 Files.copy(
-                    Paths.get("$fileImagesPiv/$name"),
-                    Paths.get("$fileImages/$name"),
+                    archivo.toPath(),
+                    imagenes.toPath().resolve(name),
                     StandardCopyOption.REPLACE_EXISTING
                 )
-
-                // Borrar archivo
-                File(fileImagesPiv, name).delete()
+                File(imgPiv, name).delete()
             }
         }
     }
