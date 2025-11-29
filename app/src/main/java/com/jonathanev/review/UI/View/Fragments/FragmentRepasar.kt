61871b5f05730b388c9c2ef80b1cbef73a8c6b32
JonathanEV <jose_jona321@hotmail.com>
2025-11-28 18:31:43 -0600

    Diseño de pantalla creado

---
+++ app/src/main/java/com/jonathanev/review/UI/View/Fragments/FragmentRepasar.kt
@@ -0,0 +1,26 @@
+package com.jonathanev.review.UI.View.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonathanev.review.R
+import com.jonathanev.review.databinding.FragmentRepasarBinding
+
+class FragmentRepasar : Fragment() {
+    private var _binding: FragmentRepasarBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRepasarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
