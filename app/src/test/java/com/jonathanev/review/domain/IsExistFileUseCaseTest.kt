e4cf895042b1f0dedcb9611937aa81e59cecbdce
JonathanEV <jona142959@gmail.com>
2026-02-12 17:53:59 -0600

    feature: creacion del IsExistFileUseCaseTest

---
+++ app/src/test/java/com/jonathanev/review/domain/IsExistFileUseCaseTest.kt
@@ -0,0 +1,37 @@
+package com.jonathanev.review.domain
+
+import com.jonathanev.review.domain.model.GuideDomainModel
+import com.jonathanev.review.domain.model.GuideVersion
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class IsExistFileUseCaseTest {
+    private lateinit var cachedGuides: List<GuideDomainModel>
+    private lateinit var isExistFileUseCase: IsExistFileUseCase
+
+    @Before
+    fun setUp() {
+        cachedGuides = listOf(
+            GuideDomainModel(GuideVersion.V2, "Abap", ""),
+            GuideDomainModel(GuideVersion.V2, "Kotlin", "")
+        )
+
+        isExistFileUseCase = IsExistFileUseCase()
+    }
+
+    @Test
+    fun the_guide_does_not_exist(){
+        val response = isExistFileUseCase.invoke(cachedGuides, "Testing")
+
+        assertFalse(response)
+    }
+
+    @Test
+    fun the_guide_exists(){
+        val response = isExistFileUseCase.invoke(cachedGuides, "Kotlin")
+
+        assertTrue(response)
+    }
+}
