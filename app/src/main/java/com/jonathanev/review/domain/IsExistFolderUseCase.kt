7fcd728660e1a4f330d1508165e3b61cdeaf7464
JonathanEV <jona142959@gmail.com>
2026-02-13 08:55:56 -0600

    chore: ajuste en isExistFolderUseCase

--- app/src/main/java/com/jonathanev/review/domain/IsExistFolderUseCase.kt
+++ app/src/main/java/com/jonathanev/review/domain/IsExistFolderUseCase.kt
@@ -1,12 +1,13 @@
 package com.jonathanev.review.domain
 
+import com.jonathanev.review.domain.repository.FolderRepository
 import javax.inject.Inject
 
 class IsExistFolderUseCase @Inject constructor(
-    private val getFoldersWithNumGuidesUseCase: GetFoldersWithNumGuidesUseCase
+    private val folderRepository: FolderRepository
 ) {
     operator fun invoke(name: String): Boolean {
-        val foldersDomain = getFoldersWithNumGuidesUseCase.invoke()
+        val foldersDomain = folderRepository.getFolders()
         val folderDomainModel = foldersDomain.find { it.folder.name == name }
         return folderDomainModel != null
     }
