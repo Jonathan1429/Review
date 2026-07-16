0d093271ed3a448ec35bd58f2be83fa9e28c4a9b
JonathanEVMac <jona142959@gmail.com>
2026-07-16 15:55:33 -0600

    feature: Previsualizacion de FilterTypeItem.kt

--- app/src/main/java/com/jonathanev/review/ui/components/FilterTypeItem.kt
+++ app/src/main/java/com/jonathanev/review/ui/components/FilterTypeItem.kt
@@ -6,10 +6,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.jonathanev.review.ui.mapper.toDrawable
 import com.jonathanev.review.ui.model.ContentType
-import kotlin.collections.forEach
+import com.jonathanev.review.ui.preview.providers.FilterTypeItemProv
+import com.jonathanev.review.ui.preview.providers.FilterTypeItemProvider
+
+//@DevicePreviews
+@Composable
+fun PreviewFilterTypeItem(
+    @PreviewParameter(FilterTypeItemProvider::class) data: FilterTypeItemProv
+) {
+    FilterTypeItem(
+        mediaForSelected = data.media,
+        mediaSelected = data.mediaSelected,
+        onFilterClicked = {}
+    )
+}
 
 @Composable
 fun FilterTypeItem(
