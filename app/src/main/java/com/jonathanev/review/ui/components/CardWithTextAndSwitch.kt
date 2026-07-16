ad2095f7df36baa9fcf5f2c8b2d898f6894e4623
JonathanEVMac <jona142959@gmail.com>
2026-07-16 13:29:56 -0600

    feature: Previsualizacion de CardWithTextAndSwitch.kt

--- app/src/main/java/com/jonathanev/review/ui/components/CardWithTextAndSwitch.kt
+++ app/src/main/java/com/jonathanev/review/ui/components/CardWithTextAndSwitch.kt
@@ -18,12 +18,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jonathanev.review.R
+import com.jonathanev.review.ui.preview.DevicePreviews
 import com.jonathanev.review.ui.theme.Teal200
 import com.jonathanev.review.ui.theme.TextGray
 import com.jonathanev.review.ui.theme.cardStepBackground
+
+@DevicePreviews
+@Composable
+fun PreviewCardWithTextAndSwitch() {
+    CardWithTextAndSwitch(
+        isChecked = false,
+        onCheckedChange = {}
+    )
+}
 
 @Composable
 fun CardWithTextAndSwitch(
---
+++ app/src/test/java/com/jonathanev/review/ui/components/CardWithTextAndSwitchScreenshotTest.kt
@@ -0,0 +1,103 @@
+package com.jonathanev.review.ui.components
+
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.unit.dp
+import com.github.takahirom.roborazzi.captureRoboImage
+import com.jonathanev.review.ui.preview.providers.DataMediaContentPagerProvider
+import com.jonathanev.review.ui.preview.providers.MediaContentPagerProvider
+import com.jonathanev.review.ui.theme.ReviewTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+import kotlin.intArrayOf
+import kotlin.sequences.forEach
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(
+    sdk = [34],
+    instrumentedPackages = ["androidx.loader.content"]
+)
+class CardWithTextAndSwitchScreenshotTest(
+    private val variantName: String,
+    private val themeQualifier: String,
+    private var isChecked: Boolean
+) {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+        fun data(): Collection<Array<Any>> {
+            val testCases = mutableListOf<Array<Any>>()
+            val provider = listOf(true, false)
+            provider.forEach { dataState ->
+                // MODO CLARO
+                testCases.add(
+                    arrayOf(
+                        "${dataState}_light",
+                        "notnight",
+                        dataState
+                    )
+                )
+
+                // MODO OSCURO
+                testCases.add(
+                    arrayOf(
+                        "${dataState}_dark",
+                        "night",
+                        dataState
+                    )
+                )
+            }
+
+            return testCases
+        }
+    }
+
+    @Test
+    fun captureCardWithTextAndSwitchVariant() {
+        RuntimeEnvironment.setQualifiers(themeQualifier)
+
+        composeTestRule.setContent {
+            ReviewTheme {
+                Column {
+                    Surface(
+                        color = MaterialTheme.colorScheme.tertiaryContainer,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "PREVIEW: Checked: $isChecked",
+                            style = MaterialTheme.typography.labelMedium,
+                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                            color = MaterialTheme.colorScheme.onTertiaryContainer
+                        )
+                    }
+
+                    CardWithTextAndSwitch(
+                        isChecked = isChecked,
+                        onCheckedChange = {}
+                    )
+                }
+            }
+        }
+
+        composeTestRule.onRoot().captureRoboImage(
+            filePath = "build/outputs/roborazzi/text_and_switch_viewer/${variantName}.png"
+        )
+    }
+}
