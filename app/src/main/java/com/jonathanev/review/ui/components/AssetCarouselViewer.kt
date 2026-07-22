3a51076978da4bd7b232d77a71540051b211566e
JonathanEVMac <jona142959@gmail.com>
2026-07-21 20:08:50 -0600

    chore: Navegacion y flujo completo en pregunta con Texto

--- app/src/main/java/com/jonathanev/review/ui/components/AssetCarouselViewer.kt
+++ app/src/main/java/com/jonathanev/review/ui/components/AssetCarouselViewer.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -85,6 +86,12 @@ fun AssetCarouselViewer(
         rememberPagerState(initialPage = currentPosContent, pageCount = { assets.size })
     val scope = rememberCoroutineScope()
     val lazyRowState = rememberLazyListState()
+
+    LaunchedEffect(currentPosContent) {
+        if (assets.isNotEmpty() && pagerState.currentPage != currentPosContent) {
+            pagerState.scrollToPage(currentPosContent)
+        }
+    }
 
     Column(
         modifier = Modifier
