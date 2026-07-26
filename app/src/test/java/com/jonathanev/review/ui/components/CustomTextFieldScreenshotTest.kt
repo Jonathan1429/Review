8e22e6e866a939f7bc271127625e908ff87e8316
JonathanEVMac <jona142959@gmail.com>
2026-07-25 23:59:38 -0600

    chore: Creacion de tareas para ejecucion de roborazzi y kover sin que se pisen

---
+++ app/src/test/java/com/jonathanev/review/ui/components/CustomTextFieldScreenshotTest.kt
@@ -0,0 +1,106 @@
+package com.jonathanev.review.ui.components
+
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.unit.dp
+import com.jonathanev.review.ui.preview.providers.CustomTextFieldDataProvider
+import com.jonathanev.review.ui.preview.providers.PropertiesTF
+import com.jonathanev.review.ui.theme.ReviewTheme
+import com.jonathanev.review.utils.captureTestOutput
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(
+    sdk = [34],
+    instrumentedPackages = ["androidx.loader.content"]
+)
+class CustomTextFieldScreenshotTest(
+    private val variantName: String,
+    private val themeQualifier: String,
+    private val dataState: PropertiesTF
+) {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+        fun data(): Collection<Array<Any>> {
+            val provider = CustomTextFieldDataProvider()
+            val testCases = mutableListOf<Array<Any>>()
+
+            provider.values.forEach { dataState ->
+                // MODO CLARO
+                testCases.add(
+                    arrayOf(
+                        "${dataState}_light",
+                        "notnight",
+                        dataState
+                    )
+                )
+
+                // MODO OSCURO
+                testCases.add(
+                    arrayOf(
+                        "${dataState}_dark",
+                        "night",
+                        dataState
+                    )
+                )
+            }
+
+            return testCases
+        }
+    }
+
+    @Test
+    fun captureVariant() {
+        RuntimeEnvironment.setQualifiers(themeQualifier)
+
+        composeTestRule.setContent {
+            ReviewTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Column {
+                        Surface(
+                            color = MaterialTheme.colorScheme.tertiaryContainer,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "PREVIEW: $dataState - ${dataState::class.simpleName}",
+                                style = MaterialTheme.typography.labelMedium,
+                                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                                color = MaterialTheme.colorScheme.onTertiaryContainer
+                            )
+                        }
+
+                        CustomTextField(
+                            name = dataState.name,
+                            label = dataState.label,
+                            onValueChange = {}
+                        )
+                    }
+                }
+            }
+        }
+
+        composeTestRule.onRoot().captureTestOutput(
+            testClassName = this::class.java.simpleName,
+            variantName = variantName
+        )
+    }
+}
