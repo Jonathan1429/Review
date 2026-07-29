ac47311228329a3d3154d8c8b021fa9c6463ec90
JonathanEVMac <jona142959@gmail.com>
2026-07-29 13:49:58 -0600

    chore: GetPreviewQuestionsUseCaseTest test ajustado con la nueva logica

--- app/src/test/java/com/jonathanev/review/domain/GetPreviewQuestionsUseCaseTest.kt
+++ app/src/test/java/com/jonathanev/review/domain/GetPreviewQuestionsUseCaseTest.kt
@@ -3,15 +3,13 @@ package com.jonathanev.review.domain
 import com.jonathanev.review.domain.model.QAItemDomain
 import com.jonathanev.review.domain.model.QuestionContentDomain
 import com.jonathanev.review.domain.model.QuestionItemDomain
-import com.jonathanev.review.domain.model.RelativeGuidePath
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Before
-import kotlin.test.Ignore
+import org.junit.Test
 
 class GetPreviewQuestionsUseCaseTest {
     @MockK
@@ -24,65 +22,60 @@ class GetPreviewQuestionsUseCaseTest {
         useCase = GetPreviewQuestionsUseCase(setPintarTextosUseCase)
     }
 
-    @Ignore("No")
+    @Test
     fun `should take first text from question and count images from question and answer`() =
         runTest {
+            // -------- Given --------
+            val questionItem1 = QuestionContentDomain.Text("Primer texto", emptyList())
+            val questionItem2 = QuestionContentDomain.Image("uri", "img.png")
+            val answerItem1 = QuestionContentDomain.Text("Segundo texto", emptyList())
+            val answerItem2 = QuestionContentDomain.Image("uri2", "img2.png")
 
-        // -------- Given --------
-
-        val questionItem1 = QuestionContentDomain.Text("Primer texto", emptyList())
-        val questionItem2 = QuestionContentDomain.Image("uri", "img.png")
-        val answerItem1 = QuestionContentDomain.Image("uri2", "img2.png")
-        val answerItem2 = QuestionContentDomain.Text("Segundo texto", emptyList())
-
-        // Configuramos qué devuelve el use case para cada item
+            // Configuramos qué devuelve el use case para cada item
             coEvery {
                 setPintarTextosUseCase.invoke(questionItem1)
-        } returns questionItem1
+            } returns questionItem1
 
             coEvery {
                 setPintarTextosUseCase.invoke(questionItem2)
-        } returns questionItem2
-
-            coEvery {
-                setPintarTextosUseCase.invoke(answerItem1)
-        } returns answerItem1
+            } returns questionItem2
 
             coEvery {
                 setPintarTextosUseCase.invoke(answerItem2)
-        } returns answerItem2
+            } returns answerItem2
 
-        val qaItem1 = QAItemDomain(
-            question = QuestionItemDomain(listOf(questionItem1, questionItem2, questionItem1)),
-            answer = QuestionItemDomain(listOf(answerItem1, answerItem2, answerItem1))
-        )
+            coEvery {
+                setPintarTextosUseCase.invoke(answerItem1)
+            } returns answerItem1
 
-        val qaItem2 = QAItemDomain(
-            question = QuestionItemDomain(listOf(questionItem2)),
-            answer = QuestionItemDomain(listOf(answerItem1))
-        )
+            val qaItem1 = QAItemDomain(
+                question = QuestionItemDomain(listOf(questionItem1, questionItem2, questionItem1)),
+                answer = QuestionItemDomain(listOf(answerItem2, answerItem1))
+            )
 
-        val relativePath = RelativeGuidePath("path")
+            val qaItem2 = QAItemDomain(
+                question = QuestionItemDomain(listOf(questionItem2)),
+                answer = QuestionItemDomain(listOf(answerItem2))
+            )
 
-        // -------- When --------
-
+            // -------- When --------
             val result = useCase.invoke(listOf(qaItem1, qaItem2))
 
-        // -------- Then --------
+            // -------- Then --------
+            assertEquals(2, result.size)
 
-        assertEquals(2, result.size)
+            val previewOne = result.first()
+            val previewTwo = result[1]
 
-        val previewOne = result.first()
-        val previewTwo = result[1]
+            assertEquals(
+                /* expected = */ "Primer texto",
+                /* actual = */ previewOne.question.text
+            )
 
-        assertTrue(previewOne.question is QuestionContentDomain.Text)
-        assertEquals(
-            "Primer texto",
-            (previewOne.question as QuestionContentDomain.Text).text
-        )
+            assertEquals(3, previewOne.noTexts)
+            assertEquals(2, previewOne.noImages)
 
-        // 1 imagen en pregunta + 1 en respuesta
-        assertEquals(3, previewOne.noImages)
-        assertEquals(2, previewTwo.noImages)
-    }
+            assertEquals(0, previewTwo.noTexts)
+            assertEquals(2, previewTwo.noImages)
+        }
 }
