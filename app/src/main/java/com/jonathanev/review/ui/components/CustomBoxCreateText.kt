c13cf5fe727d456d48613160426c370701911e5f
JonathanEVMac <jona142959@gmail.com>
2026-08-01 20:09:03 -0600

    chore: Eliminacion de preview del CustomBoxCreateText

--- app/src/main/java/com/jonathanev/review/ui/components/CustomBoxCreateText.kt
+++ app/src/main/java/com/jonathanev/review/ui/components/CustomBoxCreateText.kt
@@ -11,19 +11,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.jonathanev.review.R
-
-@Preview
-@Composable
-fun PreviewCustomBoxCreateText() {
-    CustomBoxCreateText(
-        textValue = TextFieldValue("asdf"),
-        hint = false,
-        onTextValueChange = {}
-    )
-}
 
 @Composable
 fun CustomBoxCreateText(
