9f1f68b21e7404163efeae2056f4038c03b111ee
JonathanEVMac <jona142959@gmail.com>
2026-08-09 09:28:46 -0600

    chore: Texto del item de la guia con limite

--- app/src/main/java/com/jonathanev/review/ui/components/ItemGuide.kt
+++ app/src/main/java/com/jonathanev/review/ui/components/ItemGuide.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -78,14 +78,17 @@ fun ItemGuide(
                     text = guide.nameGuide,
                     color = MaterialTheme.colorScheme.onSurface,
                     fontSize = 17.sp,
-                    fontWeight = FontWeight.SemiBold
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(
                     text = guide.displayDescription,
                     color = getColorSubtitle(),
                     fontSize = 13.sp,
-                    lineHeight = 18.sp
+                    lineHeight = 18.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
